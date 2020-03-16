e48a5ad2233124ccf2b625f48afafe42451f7338
delasalle-sio-blandin-l <delasalle.sio.blandin.l@gmail.com>
2020-03-16 18:36:46 +0100

    TP2 Exo 8

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,3 +1,4 @@
+import { GameFilter } from './../game-list-filter/game-list-filter.component';
 import { Component, OnInit } from '@angular/core';
 
 import { Game } from './game';
@@ -23,29 +24,15 @@ export class GameListComponent implements OnInit {
     editor: 'The Behemoth',
     image: 'https://steamcdn-a.akamaihd.net/steam/apps/238460/header.jpg?t=1561397233',
     note: 9.8,
-    category: 'Jeu de platform multijoueur'
+    category: 'RPG'
   }, {
     id: 2,
-    name: 'BattleBlock Theater 2',
-    description: 'Shipwrecked. Captured. Betrayed. Forced to perform for an audience of cats? '
-      + 'Yes, all that and more when you unlock BattleBlock Theater!'
-      + ' There\'s no turning back once you\'ve started on your quest to free over 300 of your imprisoned '
-      + 'friends from evil technological cats. ',
-    editor: 'The Behemoth',
-    image: 'https://steamcdn-a.akamaihd.net/steam/apps/238460/header.jpg?t=1561397233',
-    note: 9.8,
-    category: 'Jeu de platform multijoueur'
-  }, {
-    id: 2,
-    name: 'BattleBlock Theater 2',
-    description: 'Shipwrecked. Captured. Betrayed. Forced to perform for an audience of cats? '
-      + 'Yes, all that and more when you unlock BattleBlock Theater!'
-      + ' There\'s no turning back once you\'ve started on your quest to free over 300 of your imprisoned '
-      + 'friends from evil technological cats. ',
-    editor: 'The Behemoth',
-    image: 'https://steamcdn-a.akamaihd.net/steam/apps/238460/header.jpg?t=1561397233',
-    note: 9.8,
-    category: 'Jeu de platform multijoueur'
+    name: 'Call Of Duty 32',
+    description: 'Toujours plus ... ',
+    editor: 'Activision',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/CallofDutyLogo.svg/310px-CallofDutyLogo.svg.png',
+    note: 8,
+    category: 'FPS'
   }, {
     id: 3,
     name: 'BattleBlock Theater 2',
@@ -56,7 +43,7 @@ export class GameListComponent implements OnInit {
     editor: 'The Behemoth',
     image: 'https://steamcdn-a.akamaihd.net/steam/apps/238460/header.jpg?t=1561397233',
     note: 9.8,
-    category: 'Jeu de platform multijoueur'
+    category: 'RPG'
   }, {
     id: 4,
     name: 'BattleBlock Theater 2',
@@ -67,8 +54,21 @@ export class GameListComponent implements OnInit {
     editor: 'The Behemoth',
     image: 'https://steamcdn-a.akamaihd.net/steam/apps/238460/header.jpg?t=1561397233',
     note: 9.8,
+    category: 'RPG'
+  }, {
+    id: 5,
+    name: 'BattleBlock Theater 2',
+    description: 'Shipwrecked. Captured. Betrayed. Forced to perform for an audience of cats? '
+      + 'Yes, all that and more when you unlock BattleBlock Theater!'
+      + ' There\'s no turning back once you\'ve started on your quest to free over 300 of your imprisoned '
+      + 'friends from evil technological cats. ',
+    editor: 'The Behemoth',
+    image: 'https://steamcdn-a.akamaihd.net/steam/apps/238460/header.jpg?t=1561397233',
+    note: 9.8,
     category: 'Jeu de platform multijoueur'
   }];
+
+  filteredEntities = this.entities;
 
   constructor() { }
 
@@ -95,6 +95,13 @@ export class GameListComponent implements OnInit {
 
   onActionClick(action: GameActions, game: Game) {
     alert(`${['follow', 'share', 'buy'][action]} the game nammed ${game.name}`);
+  }
+
+  onFilter(filterForm: GameFilter) {
+    this.filteredEntities = this.entities
+        .filter(e => (!filterForm.name || e.name.toLocaleLowerCase().includes(filterForm.name))
+            && (!filterForm.category || e.category === filterForm.category)
+            && (!filterForm.editor || e.editor.toLowerCase().includes(filterForm.editor)));
   }
 
 }
