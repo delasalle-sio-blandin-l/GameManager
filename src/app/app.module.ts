416715ed27e24dfa371eb1d24f82615f717c10f5
delasalle-sio-blandin-l <delasalle.sio.blandin.l@gmail.com>
2020-05-26 16:37:41 +0200

    Routing Exo 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GameListModule } from './game-list/game-list.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
