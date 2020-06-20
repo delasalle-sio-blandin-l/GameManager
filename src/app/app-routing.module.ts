d5bf2c9a85702b763493f9679755930dedbaf61b
delasalle-sio-blandin-l <delasalle.sio.blandin.l@gmail.com>
2020-06-20 21:49:49 +0200

    Ajout de routes pas complètement fonctionnelle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { GameListComponent } from './game-list/game-list.component';
 import { GameDetailsComponent } from './game-details/game-details.component';
+import { GameFormComponent } from './game-form/game-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'product', pathMatch: 'full'},
   { path: 'product', component: GameListComponent },
   { path: 'product/:productId', component: GameDetailsComponent },
 
+  { path: 'product/new', component: GameFormComponent },
+  { path: 'product/:productId/edit', component: GameFormComponent },
 
   { path: '**', component: NotFoundComponent},
 ];
